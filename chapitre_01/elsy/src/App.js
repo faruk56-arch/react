4be009ec62a4691c2605cac5dcf3bbe88977256e
momin <faruk373@gmail.com>
2021-04-07 16:30:00 +0200

    f

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import Steps from "./components/Steps"
 
 const tempMin = -20;
 const tempMax = 40;
@@ -12,7 +13,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
+
         <div className="row">
+          <Steps/>
+
           <p>Heart : {heartMin}</p>
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
---
+++ chapitre_01/elsy/src/components/Steps.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+ 
+class Steps extends React.Component {
+	name = "John";
+
+  render() {
+    return (
+			<div className="box col-sm-3 col-6">
+                <span className="material-icons fontSize = 200 color = black">
+                directions_walk
+
+                </span>     
+				
+			</div>
+		)
+  };
+}
+
+export default Steps;
