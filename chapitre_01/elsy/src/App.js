2c94492de3e69a6a4fe09445dacbf0aae9986d73
momin <faruk373@gmail.com>
2021-04-08 16:58:51 +0200

    f

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './styles/global.css';
-import Box from "./components/Box";
+import Box from './components/Box';
+
 
 const tempMin = -20;
 const tempMax = 40;
@@ -12,22 +13,20 @@ const stepsMax = 50000;
 class App extends React.Component {
   render() {
     return (
+
+      
       <div className="container-fluid">
-
         <div className="row">
-
-          <Box icon="local_drink" color="red" value="1.5" unit="L" />
-          <Box icon="directions_walk" color="black" value="3000" unit="steps" />
-          <Box icon="favorite" color="red" value="120" unit="bpm" />
-          <Box icon="wb_sunny" color="yellow" value="-10" unit="°c" />
-
-          <h1>Heart : {heartMin}</h1>
-          <h1>Temperature : {tempMin}</h1>
-          <h1>Steps : {stepsMin}</h1>
-
+          <Box icon = "local_drink" color = "green" value = "1.5" unit = "L"/>
+          <Box icon = "directions_walk" color = "black" value = "3000" unit = "steps"/>
+          <Box icon = "favorite" color = "red" value = "120" unit = "bpm"/>
+          <Box icon = "wb_sunny" color = "yellow" value = "-10" unit = "°c"/>
+          
+          <p>Heart: {heartMin}</p>
+          <p>Temperature: {tempMin}</p>
+          <p>Steps: {stepsMin}</p>
         </div>
-
-
+        
       </div>
     );
   }
