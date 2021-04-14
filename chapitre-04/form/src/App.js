9b0eaa4fc0ee4db528f19a040042c6b5f1c2b1ff
momin <faruk373@gmail.com>
2021-04-14 16:49:53 +0200

    f

--- chapitre-04/form/src/App.js
+++ chapitre-04/form/src/App.js
@@ -8,27 +8,59 @@ class App extends React.Component {
 
   constructor() {
     super()
+    // this.state = {
+    //   input: "",
+    //   errors: ""
+    // }
     this.state = {
-      input: "",
-      errors: ""
+      password: "",
+      email: "",
+      submit: ""
+    }
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.validEmail = this.validEmail.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this)
+  }
+
+
+  handleSubmit(e){
+    this.setState({
+      submit:e.target.value
+    })
+  }
+
+  handleEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+  validEmail() {
+    if (this.state.email.length < 6) {
+      return "form-control is-invalid"
+    } else {
+      return "form-control is-valid"
     }
 
   }
 
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
 
+  }
 
   handleValidation() {
-    const input = {};
-    const errors = {};
-    if(input){
-      return " form-control is-valid"
-    } else {
+
+    if (this.state.password.length < 6) {
       return "form-control is-invalid"
+    } else {
+      return "form-control is-valid"
     }
 
-
   }
-
 
   render() {
 
@@ -39,11 +71,21 @@ class App extends React.Component {
         <form>
           <div className=" mb-3">
             <label for="exampleInputEmail1" className="form-label warning">Email address</label>
-            <input  type="email" className={this.handleValidation()}  id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="john@gmail.com" />
+            <input id="exampleInputEmail1"
+              className={this.validEmail()}
+              onChange={this.handleEmail}
+              aria-describedby="emailHelp"
+              placeholder="john@gmail.com" />
           </div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" className={this.handleValidation()} id="exampleInputPassword1" placeholder="Password" />
+
+            <input type="password"
+              className={this.handleValidation()}
+              onChange={this.handlePassword}
+              id="exampleInputPassword1" value={this.state.password}
+              placeholder="Password" />
+
           </div>
           <div class="mb-3 form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
